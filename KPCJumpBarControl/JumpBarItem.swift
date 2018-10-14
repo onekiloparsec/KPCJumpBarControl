b135b700a8a4c8258340d54139f92b192f7c929a
Cédric Foellmi <cedric@onekilopars.ec>
2018-10-14 14:02:59 +0200

    Not what we were up to a year ago… but let restart with items

--- KPCJumpBarControl/JumpBarItem.swift
+++ KPCJumpBarControl/JumpBarItem.swift
@@ -10,10 +10,33 @@ import AppKit
 
 // NSObjectProtocol and NSObject are necessary for JumpBarItems to be set as representedObject of NSMenuItems.
 
-public protocol JumpBarItem: class, NSObjectProtocol {
+public protocol JumpBarItemProtocol: class, NSObjectProtocol {
     var title: String { get }
     var icon: NSImage? { get }
-    var children: [JumpBarItem]? { get }
+    var children: [JumpBarItemProtocol]? { get }
+    var parent: JumpBarItemProtocol? { get }
     var isSeparator: Bool { get }
+    //    var segmentIndexPath: IndexPath { get }
 }
 
+open class JumpBarItem: NSObject, JumpBarItemProtocol {
+    open var title: String = ""
+    open var icon: NSImage? = nil
+    open var children: [JumpBarItemProtocol]? = nil
+    open var parent: JumpBarItemProtocol? = nil
+    open fileprivate(set) var isSeparator: Bool = false
+    
+    public init(withTitle title: String, icon: NSImage? = nil, children: [JumpBarItemProtocol]? = [], parent: JumpBarItemProtocol? = nil) {
+        self.title = title
+        self.icon = icon
+        self.children = children
+        self.parent = parent
+    }
+    
+    static public func separatorItem() -> JumpBarItem {
+        let item = JumpBarItem(withTitle: "")
+        item.isSeparator = true
+        item.children = nil
+        return item
+    }
+}
