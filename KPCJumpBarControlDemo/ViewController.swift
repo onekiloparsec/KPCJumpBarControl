9adc0e0fa2959aa9f3b9a96512976b88312c3cd7
Cédric Foellmi <cedric@onekilopars.ec>
2018-10-14 14:03:55 +0200

    Making the demo work again

--- KPCJumpBarControlDemo/ViewController.swift
+++ KPCJumpBarControlDemo/ViewController.swift
@@ -31,35 +31,35 @@ class ViewController: NSViewController, JumpBarControlDelegate {
     
     func swapItemsTree() {
         if swap == true {
-            let rootSegment = JumpBarSegment(withTitle:"path 0", icon:NSImage(named:"Oval"))
+            let rootSegment = JumpBarItem(withTitle:"path 0", icon:NSImage(named:NSImage.Name("Oval")))
             
-            let segment1Item0 = JumpBarSegment(withTitle:"path 0.0", icon:NSImage(named:"Polygon"))
-            let segment1Item1 = JumpBarSegment(withTitle:"path 0.1", icon:NSImage(named:"Rectangle"))
-            let separatorItem = JumpBarSegment.separatorItem()
-            let segment1Item2 = JumpBarSegment(withTitle:"path 0.3 - switch to another tree", icon:NSImage(named:"Triangle"))
-            
-            rootSegment.segmentChildren = [segment1Item0, segment1Item1, separatorItem, segment1Item2]
-            
-            let segment2Item0 = JumpBarSegment(withTitle:"path 0.1.0", icon:NSImage(named:"Star"))
-            let segment2Item1 = JumpBarSegment(withTitle:"path 0.1.1", icon:NSImage(named:"Spiral"))
-            
-            segment1Item1.segmentChildren = [segment2Item0, segment2Item1]
+            let segment1Item0 = JumpBarItem(withTitle:"path 0.0", icon:NSImage(named:NSImage.Name("Polygon")))
+            let segment1Item1 = JumpBarItem(withTitle:"path 0.1", icon:NSImage(named:NSImage.Name("Rectangle")))
+            let separatorItem = JumpBarItem.separatorItem()
+            let segment1Item2 = JumpBarItem(withTitle:"path 0.3 - switch to another tree", icon:NSImage(named:NSImage.Name("Triangle")))
+
+            rootSegment.children = [segment1Item0, segment1Item1, separatorItem, segment1Item2]
+
+            let segment2Item0 = JumpBarItem(withTitle:"path 0.1.0", icon:NSImage(named:NSImage.Name("Star")))
+            let segment2Item1 = JumpBarItem(withTitle:"path 0.1.1", icon:NSImage(named:NSImage.Name("Spiral")))
+
+            segment1Item1.children = [segment2Item0, segment2Item1]
             
             self.jumpBar?.useItemsTree([rootSegment])
         }
         else {
-            let rootSegment0 = JumpBarSegment(withTitle:"path 0", icon:NSImage(named:"Rectangle"))
-            let rootSegment1 = JumpBarSegment(withTitle:"path 1", icon:NSImage(named:"Star"))
-            
-            let segment1Item0 = JumpBarSegment(withTitle:"path 1.0", icon:NSImage(named:"Polygon"))
-            rootSegment1.segmentChildren = [segment1Item0]
-            
-            let segment2Item0 = JumpBarSegment(withTitle:"path 1.0.0", icon:NSImage(named:"Spiral"))
-            segment1Item0.segmentChildren = [segment2Item0]
-            
-            let segment3Item0 = JumpBarSegment(withTitle:"path 1.0.0.0 - switch to another tree", icon:NSImage(named:"Triangle"))
-            segment2Item0.segmentChildren = [segment3Item0]
-            
+            let rootSegment0 = JumpBarItem(withTitle:"path 0", icon:NSImage(named:NSImage.Name("Rectangle")))
+            let rootSegment1 = JumpBarItem(withTitle:"path 1", icon:NSImage(named:NSImage.Name("Star")))
+
+            let segment1Item0 = JumpBarItem(withTitle:"path 1.0", icon:NSImage(named:NSImage.Name("Polygon")))
+            rootSegment1.children = [segment1Item0]
+
+            let segment2Item0 = JumpBarItem(withTitle:"path 1.0.0", icon:NSImage(named:NSImage.Name("Spiral")))
+            segment1Item0.children = [segment2Item0]
+
+            let segment3Item0 = JumpBarItem(withTitle:"path 1.0.0.0 - switch to another tree", icon:NSImage(named:NSImage.Name("Triangle")))
+            segment2Item0.children = [segment3Item0]
+
             self.jumpBar?.useItemsTree([rootSegment0, rootSegment1])
         }
     }
